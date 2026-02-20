d0a92df6dd946ea4cc27d9eebe66ab1b5137e35b
MarFri8 <111853946+MarFri8@users.noreply.github.com>
2026-02-20 13:56:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -53,58 +53,40 @@ app.post("/products", async (req, res) => {
         console.error(error);
         res.status(500).json({ error: error.message });
     }
-    /*  try {
-        const { name, price, stock } = req.body;
-        const [result] = await db
-            .promise()
-            .query("INSERT INTO products (name, price, stock) VALUES (?,?,?)", [
-                name,
-                price,
-                stock,
-            ]);
-        res.status(201).json({
-            id: result.insertId,
-            name,
-            price,
-            stock,
-        });
-    } catch (err) {
-        res.status(500).json({ error: "Kunde inte lägga till produkt" });
-    } */
 });
 
 app.post("/orders/", async (req, res) => {
-  const { id_customer, total_amount, id_product, order_qty, payment_method } =
-    req.body;
-  try {
-    /// Först skapa ordern i orders tabellen.
-    const [result] = await db
-      .promise()
-      .query(`INSERT INTO orders (id_customer, total_amount) VALUES (?, ?)`, [
-        id_customer,
-        total_amount,
-      ]);
-    /// Använder de id som används i orders tabell till att skapa orderproduct inserts.
-    const id_order = result.insertId;
-    /// Skapa orderproduct insert.
-    await db
-      .promise()
-      .query(
-        `INSERT INTO orderproduct (id_order, id_product, order_qty, payment_method) VALUES (?, ?, ?, ?)`,
-        [id_order, id_product, order_qty, payment_method],
-      );
-    res.status(201).json({ message: "Order skapad", id_order });
-  } catch (error) {
-    res.status(500).json({ error: "Kunde inte skapa order" });
-  }
-  //exempel på request i Postman
-  //   {
-  //   "id_customer": 1,
-  //   "total_amount": 12999,
-  //   "id_product": 1,
-  //   "order_qty": 1,
-  //   "payment_method": "card"
-  // }
+    const { id_customer, total_amount, id_product, order_qty, payment_method } =
+        req.body;
+    try {
+        /// Först skapa ordern i orders tabellen.
+        const [result] = await db
+            .promise()
+            .query(
+                `INSERT INTO orders (id_customer, total_amount) VALUES (?, ?)`,
+                [id_customer, total_amount],
+            );
+        /// Använder de id som används i orders tabell till att skapa orderproduct inserts.
+        const id_order = result.insertId;
+        /// Skapa orderproduct insert.
+        await db
+            .promise()
+            .query(
+                `INSERT INTO orderproduct (id_order, id_product, order_qty, payment_method) VALUES (?, ?, ?, ?)`,
+                [id_order, id_product, order_qty, payment_method],
+            );
+        res.status(201).json({ message: "Order skapad", id_order });
+    } catch (error) {
+        res.status(500).json({ error: "Kunde inte skapa order" });
+    }
+    //exempel på request i Postman
+    //   {
+    //   "id_customer": 1,
+    //   "total_amount": 12999,
+    //   "id_product": 1,
+    //   "order_qty": 1,
+    //   "payment_method": "card"
+    // }
 });
 
 /* 
@@ -114,58 +96,59 @@ READ
 /* Product Search */
 
 app.get("/products/search", async (req, res) => {
-  const { q } = req.query;
+    const { q } = req.query;
 
-  if (!q) {
-    return res.status(400).json({ error: "Search query is required" });
-  }
+    if (!q) {
+        return res.status(400).json({ error: "Search query is required" });
+    }
 
-  try {
-    const [rows] = await db
-      .promise()
-      .query("SELECT * FROM products WHERE name LIKE ?" /* AND stock > 0 */, [
-        `%${q}%`,
-      ]);
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: "Search failed" });
-  }
+    try {
+        const [rows] = await db
+            .promise()
+            .query(
+                "SELECT * FROM products WHERE name LIKE ?" /* AND stock > 0 */,
+                [`%${q}%`],
+            );
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: "Search failed" });
+    }
 });
 
 ///Hämta alla produkter.
 app.get("/products", async (req, res) => {
-  try {
-    const [rows] = await db.promise().query("SELECT * FROM products");
+    try {
+        const [rows] = await db.promise().query("SELECT * FROM products");
 
-    if (rows.length === 0)
-      return res.status(404).json({ message: "Not found" });
+        if (rows.length === 0)
+            return res.status(404).json({ message: "Not found" });
 
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: "Fetch failed" });
-  }
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: "Fetch failed" });
+    }
 });
 
 //Få detaljerad info om enskild produkt,
 //J
 app.get("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const [rows] = await db.promise().query(
-      `SELECT products.*, productdescription.desc, productdescription.specs, productdescription.image
+    const { id } = req.params;
+    try {
+        const [rows] = await db.promise().query(
+            `SELECT products.*, productdescription.desc, productdescription.specs, productdescription.image
        FROM products 
        JOIN productdescription ON products.id_product = productdescription.id_product
        WHERE products.id_product = ?`,
-      [id],
-    );
+            [id],
+        );
 
         if (rows.length === 0)
             return res.status(404).json({ message: "Not found" });
 
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: "Fetch failed" });
-  }
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: "Fetch failed" });
+    }
 });
 
 // Söka efter produkter med kategeri
@@ -212,56 +195,15 @@ app.get("/orders/:id", async (req, res) => {
 /* Get Avaiable Products */
 
 app.get("/products", async (req, res) => {
-  try {
-    const [rows] = await db
-      .promise()
-      .query("SELECT * FROM products WHERE stock > 0");
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: "Databasfel" });
-  }
-});
-
-/* app.get("/products", async (req, res) => {
     try {
-        // const [dbNameRows] = await db.promise().query("SELECT DATABASE() AS db");
-        // console.log("Connected DB:", dbNameRows[0]?.db);
-        // const [tables] = await db.promise().query("SHOW TABLES");
-        // console.log("SHOW TABLES:", tables);
-        const [rows] = await db.promise().query("SELECT * FROM products");
+        const [rows] = await db
+            .promise()
+            .query("SELECT * FROM products WHERE stock > 0");
         res.json(rows);
     } catch (error) {
-        console.error("Error vid hämtning av produkter:", error);
-        res.status(500).json({ error: "Databas fel/error" });
+        res.status(500).json({ error: "Databasfel" });
     }
 });
-
-app.get("/customer", async (req, res) => {
-    try {
-        // const [dbNameRows] = await db.promise().query("SELECT DATABASE() AS db");
-        // console.log("Connected DB:", dbNameRows[0]?.db);
-        // const [tables] = await db.promise().query("SHOW TABLES");
-        // console.log("SHOW TABLES:", tables);
-        const [rows] = await db.promise().query("SELECT * FROM customer");
-        res.json(rows);
-    } catch (error) {
-        console.error("Error vid hämtning av kunder:", error);
-        res.status(500).json({ error: "Databas fel/error" });
-    }
-});
-app.get("/order", async (req, res) => {
-    try {
-        // const [dbNameRows] = await db.promise().query("SELECT DATABASE() AS db");
-        // console.log("Connected DB:", dbNameRows[0]?.db);
-        // const [tables] = await db.promise().query("SHOW TABLES");
-        // console.log("SHOW TABLES:", tables);
-        const [rows] = await db.promise().query("SELECT * FROM `order`");
-        res.json(rows);
-    } catch (error) {
-        console.error("Error vid hämtning av ordrar:", error);
-        res.status(500).json({ error: "Databas fel/error" });
-    }
-}); */
 
 /* 
 UPDATE 
